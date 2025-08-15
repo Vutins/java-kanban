d366aff32e041cc89d09eec8af50aed04976bc1c
Nikulshina Polina <nikulschina-p@yandex.ru>
2025-08-15 15:10:30 +0400

    fix codeStyle_1

--- src/managers/InMemoryTaskManager.java
+++ src/managers/InMemoryTaskManager.java
@@ -168,7 +168,7 @@ public class InMemoryTaskManager implements TaskManager {
 
     @Override
     public void removeSubtaskById(Integer id) throws IOException {
-        if(taskMaster.get(id) != null) {
+        if (taskMaster.get(id) != null) {
             epicMaster.get(subtaskMaster.get(id).getEpicId()).getEpicSubtasks().remove(id);
             historyManager.remove(id);
             subtaskMaster.remove(id);
