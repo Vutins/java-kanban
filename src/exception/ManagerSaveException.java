7e610b36064da813e97c02d8473c8f828271baec
Nikulshina Polina <nikulschina-p@yandex.ru>
2025-08-28 16:14:37 +0400

    fix constructor in ManagerSaveException

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -2,9 +2,7 @@ package exception;
 
 public class ManagerSaveException extends RuntimeException {
 
-    String message;
-
-    public ManagerSaveException(String message) {
-        this.message = message;
+    public ManagerSaveException(final String message) {
+        super(message);
     }
 }
