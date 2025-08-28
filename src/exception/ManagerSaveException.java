20e87d5980fcb1a904c74193406acc18cdc435bf
Nikulshina Polina <nikulschina-p@yandex.ru>
2025-08-28 16:02:48 +0400

    fix codeStyle

--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -4,7 +4,7 @@ public class ManagerSaveException extends RuntimeException {
 
     String message;
 
-    public ManagerSaveException (String message) {
+    public ManagerSaveException(String message) {
         this.message = message;
     }
 }
